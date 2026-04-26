f319e9a47e7c9bc72f5d52d121dcc810a09c3bea
Rudransh mishra <99524492+rudranshmishra19@users.noreply.github.com>
2026-04-26 16:17:21 +0530

    coded string examples

---
+++ python_string/multiline_string.py
@@ -0,0 +1,64 @@
+# i=10
+# s=f""" it is {i}:00 PM what's going on"""
+# # print(s)
+# t=""" at this hour """
+# x=s+t
+# print(x)
+# # anothoer way of writing it 
+
+# z=" Hi it is me," +" yeah i know "
+# print(len(z))
+# z=z.strip()
+# print(len(z))
+# print(z)
+
+# check if string has digit 
+# s1="12345"
+# print(s1.isdigit())
+
+# s2="Hello"
+# print(s2.isalpha())
+
+# s3="hello123"
+# print(s3.isalnum())
+
+# s4="HELLO"
+# s4.isupper()
+# print(s4.isupper())
+
+# s5="hello"
+# print(s5.islower())
+# s="substring"
+
+# if s.startswith("sbub"):
+#     print(True)
+# if s.endswith("string"):
+#     print(True)
+
+# s="kapi"
+# r="Rudransh"
+# print(",".join(["foo","bar"]))
+
+# x="Honda"
+# z="Yes"
+# print(x,z.split(","))
+
+# x="i have yo yo oy"
+# print([x.capitalize() for x in "yo yo yo".split()])
+
+# Print a number using a format string
+# number=23
+# print("%d" % number)
+
+# number=23
+# pi=3.14158
+# print("%d" % number)
+# print("%f" %pi)
+# print("%.2f" %pi)
+# print("%s"% "hello")
+# print("%10d" % number)
+
+# numbers=123
+# print("{numbers}".format(numbers=numbers))
+
+print("this is fun".find("fun"))
---
+++ python_string/remove_last_element.py
@@ -0,0 +1,21 @@
+# my_list=[1,2,3,4]
+# del my_list[-1]
+# print(my_list)
+
+# Using my_list.pop()
+my_list =[1,2,3]
+# my_list.pop()
+# print(my_list)
+
+# i=2
+# if i in my_list:
+#     my_list.remove(i)
+# print(my_list)
+
+# my_list=list(reversed(my_list))
+# print(my_list)
+
+a=11
+b=2
+c=a//b
+print(c)
